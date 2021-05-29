664144cd58170bde2777c538e73dd40fb87699bf
Suan <suan.tay@gmail.com>
2021-05-29 12:32:26 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+"""Configuration for the bot."""
+
+import os
+
+
+class DefaultConfig:
+    """Configuration for the bot."""
+    # app :  Fly_me_bot, flymebot
+    PORT = 3979
+    APP_ID = "a1c58c19-ef5b-4dea-a517-95029899a766"
+    APP_PASSWORD = "{f4qm=PKeS*7oVGXv/o958]5K"
+    LUIS_APP_ID = "b6a19010-cb55-40fe-8507-3b98449d70a9"
+    LUIS_API_KEY = "73cc1c5bb8d0432f9c478ea1f51a0c01"
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = "switzerlandnorth.api.cognitive.microsoft.com"
+    APPINSIGHTS_INSTRUMENTATION_KEY ="ebf0e165-861e-4484-9528-403485d935a8"
+    
---
+++ helpers/luis_helper.py
@@ -0,0 +1,94 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+from enum import Enum
+from typing import Dict
+from botbuilder.ai.luis import LuisRecognizer
+from botbuilder.core import IntentScore, TopIntent, TurnContext
+
+from booking_details import BookingDetails
+
+
+class Intent(Enum):
+    BOOK_FLIGHT = "Book"
+    CANCEL = "Cancel"
+    GET_WEATHER = "GetWeather"
+    NONE_INTENT = "NoneIntent"
+
+
+def top_intent(intents: Dict[Intent, dict]) -> TopIntent:
+    max_intent = Intent.NONE_INTENT
+    max_value = 0.0
+
+    for intent, value in intents:
+        intent_score = IntentScore(value)
+        if intent_score.score > max_value:
+            max_intent, max_value = intent, intent_score.score
+
+    return TopIntent(max_intent, max_value)
+
+
+class LuisHelper:
+    @staticmethod
+    async def execute_luis_query(
+        luis_recognizer: LuisRecognizer, turn_context: TurnContext
+    ) -> (Intent, object):
+        """
+        Returns an object with preformatted LUIS results for the bot's dialogs to consume.
+        """
+        result = None
+        intent = None
+
+        try:
+            recognizer_result = await luis_recognizer.recognize(turn_context)
+
+            intent = (
+                sorted(
+                    recognizer_result.intents,
+                    key=recognizer_result.intents.get,
+                    reverse=True,
+                )[:1][0]
+                if recognizer_result.intents
+                else None
+            )
+
+            if intent == Intent.BOOK_FLIGHT.value:
+                result = BookingDetails()
+
+                # dst_city
+                to_entities = recognizer_result.entities.get("$instance", {}).get("dst_city", [])
+            
+                if len(to_entities) > 0:
+                    result.destination = to_entities[0]["text"].capitalize()
+                # ----
+                # nombre d'adults
+                adults_entities = recognizer_result.entities.get("$instance", {}).get("n_adult", [] )
+                if len(adults_entities) > 0:
+                    result.adults = adults_entities[0]["text"]
+                elif len(adults_entities)==0:
+                    result.adults = 1
+                # ----
+                # or city
+                from_entities = recognizer_result.entities.get("$instance", {}).get("or_city", [])
+                if len(from_entities) > 0:
+                    result.origin = from_entities[0]["text"].capitalize()
+                    
+                # ----
+                # This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop
+                # the Time part. TIMEX is a format that represents DateTime expressions that include some ambiguity.
+                # e.g. missing a Year.
+                # ----
+                date_entities = recognizer_result.entities.get("$instance", {}).get("str_date", [])
+                
+                if len(date_entities) > 0:
+                    datetime = date_entities[0]["text"]
+                    Recognizer = require('@microsoft/recognizers-text-date-time')
+                    result = Recognizer.recognizeDateTime(datetime, Recognizer.Culture.English);
+                    print(result)
+                    result.travel_date = date_entities[0]["text"]
+                else:
+                    result.travel_date = None
+
+        except Exception as exception:
+            print(exception)
+
+        return intent, result
