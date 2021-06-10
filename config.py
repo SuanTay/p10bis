52fae213c79e4f38f89211e2fd9d21c81ef16122
Suan <suan.tay@gmail.com>
2021-06-10 16:13:27 +0200

    config variable d'env

--- config.py
+++ config.py
@@ -9,12 +9,11 @@ import os
 class DefaultConfig:
     """Configuration for the bot."""
     # app :  Fly_me_bot, flymebot
-    PORT = 3979
-    APP_ID = "9cd88225-f5d8-4ef0-af35-28cb24f3c735"
-    APP_PASSWORD = "*O!R|z8fKX7b%!;_iG$&^nWZ[hSyx"
-    LUIS_APP_ID = "22dace73-c035-4def-a5c0-072306b41258"
-    LUIS_API_KEY = "27c8037609e64e908a3cc829433e4437" # Authoring key
-    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    PORT  = os.environ.get("port", 3979 )
+    APP_ID = os.environ.get("MicrosoftAppId", "7edb707a-eaf5-4a2b-ae9a-440bc2aa9f10")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "wj6f?f%+kB.%(Qj71jUIE{yP>54w>oD")
+    LUIS_APP_ID  = os.environ.get("LuisAppId", "22dace73-c035-4def-a5c0-072306b41258")
+    LUIS_API_KEY  = os.environ.get("LuisApiKey", "27c8037609e64e908a3cc829433e4437" ) # Authoring key
     LUIS_API_HOST_NAME ="https://luisflyme02-authoring.cognitiveservices.azure.com/" # "westeurope.api.cognitive.microsoft.com"
-    APPINSIGHTS_INSTRUMENTATION_KEY ="a3f1f698-5b10-4d5b-966d-0a2fa2006c3c"
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get("AppInsights", "9e7124b9-929c-40a4-8dcf-59cb037148ef")
     
